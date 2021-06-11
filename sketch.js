476056c47e75b2a59467f7b219f007dcedac9b02
Bhoumik00 <78673450+Bhoumik00@users.noreply.github.com>
2021-06-11 16:45:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var cat;
+
+
+function preload() {
+    //load the images here
+    
+}
+
+function setup(){
+    createCanvas(800,600);
+    //create tom and jerry sprites here
+
+    var cat = createSprites(200,30,50,30);
+   
+
+}
+
+function draw() {
+
+    background("green");
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
